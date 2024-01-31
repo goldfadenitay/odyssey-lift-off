07b956fd01975cfb1699604afc95df32edef8926
duks500 <duks500@gmail.com>
2024-01-30 23:14:49 -0600

    part #1

---
+++ client/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+// After
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
+  </React.StrictMode>
+);
